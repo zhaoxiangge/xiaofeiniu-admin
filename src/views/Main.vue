7fa44c5500e61d5873e50ae1801a81d6d33edf1c
zhaoxiangge <577584496@qq.com>
2019-01-29 18:08:45 +0800

    dajain

---
+++ src/views/Main.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an main page</h1>
+  </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/**
+ * 脚手架项目的核心配置
+ */
+
+module.exports = {
+    //WebPack开发服务器的设置
+    devServer:{
+        host:'127.0.0.1',
+        port:8091.
+    }
+}
